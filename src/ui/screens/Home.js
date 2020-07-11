54bf1b88d701675c20f0c08ea84f81bb63a84f8a
Brett Jones <brettjonesdev@gmail.com>
2020-07-10 14:53:40 -1000

    Add basic header and main screen, with generic content to allow for use as a template

---
+++ src/ui/screens/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Box } from '@material-ui/core'
+import Typography from '@material-ui/core/Typography'
+
+const Home = () => {
+  return (
+    <Box>
+      <Typography variant="h2">Content</Typography>
+    </Box>
+  )
+}
+export default Home
